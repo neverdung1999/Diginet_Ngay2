dd1fd216557d89934393982a019a31390dd3a807
Minh Duc <neverdung1999@gmail.com>
2021-05-14 17:12:52 +0700

    update file final

--- src/components/circle/Index.js
+++ src/components/circle/Index.js
@@ -1,48 +1,32 @@
+import _ from "lodash";
 import React, { useState } from "react";
 import "./styles.css";
 
-let numberRandom = Math.floor(Math.random() * 10000) + 3000;
+let numberRandom = 0;
 
 function Index(props) {
-  const { number, color } = props;
+  const { number, color, chosseOption } = props;
   const [test, setTest] = useState(false);
   const [addCss, setAddCss] = useState({
     transform: "",
   });
-  const [userWin, setUserWin] = useState("");
+  const [userWin, setUserWin] = useState([]);
+  const [statusNoti, setStatusNoti] = useState(false);
+  const [statusRank, setStatusRank] = useState(false);
+
   let count = number.length;
   let result = 0;
   let soNhan = 100 / count;
   let arrTamtest = [];
   let arrFind = [];
   let ketQua = 360 / count;
-
-  const handleSpin = () => {
-    setTest(true);
-    setTimeout(function () {
-      setTest(false);
-      numberRandom += 5000 + Math.floor(Math.random() * 1000) + 100;
-    }, 10000);
-    let onClick = "rotate(" + numberRandom + "deg)";
-    setAddCss({
-      transform: onClick,
-    });
-
-    let valueSau =
-      (360 - (numberRandom / 360 - Math.floor(numberRandom / 360)) * 360) /
-      ketQua;
-    let tronLen = Math.ceil(valueSau);
-
-    setTimeout(function () {
-      number.forEach((e, index) => {
-        if (index + 1 === tronLen) {
-          setUserWin(e);
-        }
-      });
-    }, 10000);
-  };
+  let arrDoLechTam = [];
+  let stt = false;
 
   number.forEach((e, index) => {
+    let doLechTam = 0;
+    doLechTam = ((index + 1) * soNhan * 360) / 100;
+    arrDoLechTam.push(doLechTam);
     result = soNhan * (index + 1);
     let onClick = "#" + color[index] + " 0 " + result + "%";
     arrFind.push(result);
@@ -50,9 +34,72 @@ function Index(props) {
   });
   const aaaReverse = [...arrTamtest].join();
 
-  const closeNoti = () => {
-    setUserWin("");
+  const handleSpin = () => {
+    stt = number.some((item) => _.isEmpty(item));
+    if (!stt) {
+      if (chosseOption == -1) {
+        numberRandom = Math.floor(Math.random() * 10000) + 5000 + numberRandom;
+        setTimeout(function () {
+          setTest(false);
+          props.testThuNha(false);
+          numberRandom += 5000 + Math.floor(Math.random() * 1000) + 100;
+        }, 1000);
+      } else {
+        // ---- hack game ----
+        let randomNumber = Math.floor(Math.random() * ketQua);
+        const a = 7200 - arrDoLechTam[chosseOption] + randomNumber;
+        if (a <= numberRandom) {
+          numberRandom +=
+            360 -
+            (numberRandom / 360 - Math.floor(numberRandom / 360)) * 360 +
+            7200 -
+            arrDoLechTam[chosseOption] +
+            randomNumber;
+        } else {
+          numberRandom = 7200 - arrDoLechTam[chosseOption] + randomNumber;
+        }
+
+        setTimeout(function () {
+          setTest(false);
+          props.testThuNha(false);
+        }, 1000);
+        // ---- end hack game ----
+      }
+      setTest(true);
+      props.testThuNha(true);
+      let onClick = "rotate(" + numberRandom + "deg)";
+      setAddCss({
+        transform: onClick,
+      });
+      let valueSau =
+        (360 - (numberRandom / 360 - Math.floor(numberRandom / 360)) * 360) /
+        ketQua;
+      let tronLen = Math.ceil(valueSau);
+
+      setTimeout(function () {
+        number.forEach((e, index) => {
+          if (index + 1 === tronLen) {
+            setUserWin([...userWin, e]);
+            setStatusNoti(true);
+          }
+        });
+      }, 1000);
+    } else {
+      alert("Vui lòng nhập đầy đủ thông tin");
+    }
   };
+
+  const closeNoti = () => {
+    setStatusNoti(false);
+  };
+
+  const showRank = () => {
+    setStatusRank(true);
+  };
+
+  const closeRank = () => { 
+    setStatusRank(false);
+  }
 
   return (
     <div>
@@ -102,12 +149,42 @@ function Index(props) {
           ></div>
         </div>
       )}
-      {userWin ? (
+      {statusNoti ? (
         <div className="notiAll">
           <div className="alert_success">
             <h3>Chúng ta có một người chiến thắng!</h3>
-            <p>{userWin} đã là người dành được chiến thắng</p>
+            <p>
+              {userWin[userWin.length - 1]} đã là người dành được chiến thắng
+            </p>
             <button className="btnCloseNoti" onClick={closeNoti}>
+              Đóng
+            </button>
+          </div>
+        </div>
+      ) : null}
+      {_.isEmpty(userWin) ? (
+        <button className="btnRank" onClick={() => showRank()} disabled={true}>
+          Bảng xếp hạng
+        </button>
+      ) : (
+        <button className="btnRank" onClick={() => showRank()}>
+          Bảng xếp hạng
+        </button>
+      )}
+
+      {statusRank ? (
+        <div className="notiAll">
+          <div className="tableRank">
+            <p className="p_ranking">bảng xếp hạng</p>
+            {[...new Set(userWin)].map((e, index) => {
+              return (
+                <p key={index} className="user_ranking">
+                  {" "}
+                  Người chơi {e} : đã thắng {_.countBy(userWin)[e]} lần
+                </p>
+              );
+            })}
+            <button className="btnCloseNoti" onClick={closeRank}>
               Đóng
             </button>
           </div>
--- src/components/textInput/Index.js
+++ src/components/textInput/Index.js
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
 import "./styles.css";
 import Circle from "../circle/Index";
+import _ from "lodash";
 
 function Index(props) {
   const [number, setNumber] = useState([]);
   const [color, setColor] = useState([]);
+  const [chosseOption, setChosseOption] = useState(-1);
+  const [status, setStatus] = useState(false);
+  let stt = false;
+
+  stt = number.some((item) => _.isEmpty(item));
 
   const addItem = () => {
     if (number.length > 9) {
@@ -27,14 +33,65 @@ function Index(props) {
     setNumber(arrData);
   };
 
+  const handleChangeOption = (e) => {
+    setChosseOption(e.target.value);
+  };
+
+  const okTestThu = (e) => {
+    setStatus(e);
+  };
+
   return (
     <div className="all">
       <div className="left">
         <h2>Chiếc nón diệu kì</h2>
-        <div className="icon-plus" onClick={() => addItem()}>
+        <button
+          className="icon-plus"
+          onClick={() => addItem()}
+          disabled={status}
+        >
           <i className="fas fa-plus-square"></i>
           <p>Thêm tên người chơi</p>
+        </button>
+        <div className="hackGame">
+          <p className="p_hackGame">Hack game</p>
+          {stt ? (
+            <select
+              className="selectOption"
+              onChange={(e) => handleChangeOption(e)}
+              disabled={true}
+            >
+              <option value={-1}>----- Random -----</option>
+              {number.map((e, index) => {
+                if (e !== null) {
+                  return (
+                    <option key={index} value={index}>
+                      {e}
+                    </option>
+                  );
+                }
+              })}
+            </select>
+          ) : (
+            <select
+              className="selectOption"
+              onChange={(e) => handleChangeOption(e)}
+              // disabled={statusChecked}
+            >
+              <option value={-1}>---------- Random ----------</option>
+              {number.map((e, index) => {
+                if (e !== null) {
+                  return (
+                    <option key={index} value={index}>
+                      {e}
+                    </option>
+                  );
+                }
+              })}
+            </select>
+          )}
         </div>
+
         {number.length > 0 ? (
           <div className="textInput">
             {number.map((e, index) => (
@@ -43,6 +100,8 @@ function Index(props) {
                   type="text"
                   onChange={(e) => hadleChange(e, index)}
                   placeholder={`Người chơi ${index + 1}`}
+                  value={e}
+                  required
                 />
                 <div className="icon-i" onClick={() => removeItem(index)}>
                   <i className="fas fa-times-circle"></i>
@@ -59,7 +118,12 @@ function Index(props) {
           ></div>
         )}
       </div>
-      <Circle number={number} color={color} />
+      <Circle
+        number={number}
+        color={color}
+        chosseOption={chosseOption}
+        testThuNha={okTestThu}
+      />
     </div>
   );
 }
